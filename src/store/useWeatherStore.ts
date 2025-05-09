9ec651f7d8f298bea658a81fe0a3345773a059bb
Nitin Piparava <ndpiparava@gmail.com>
2025-05-09 18:56:12 +0200

    WEATHER-000 feat: Add weather and local stores

---
+++ src/store/useWeatherStore.ts
@@ -0,0 +1,36 @@
+import {create} from 'zustand';
+import {persist} from 'zustand/middleware';
+
+import {DayWeatherType} from '@Weather/types/weather';
+
+type WeatherStoreType = {
+  weatherList: DayWeatherType[];
+  addWeather: (data: DayWeatherType) => void;
+  removeWeather: (cityName: string) => void;
+  clearWeather: () => void;
+};
+
+export const useWeatherStore = create<WeatherStoreType>()(
+  persist(
+    (set, get) => ({
+      weatherList: [],
+      addWeather: data => {
+        const existing = get().weatherList.find(w => w.name === data.name);
+        if (!existing) {
+          set(state => ({weatherList: [...state.weatherList, data]}));
+        }
+      },
+      removeWeather: cityName => {
+        set(state => ({
+          weatherList: state.weatherList.filter(w => w.name !== cityName),
+        }));
+      },
+      clearWeather: () => {
+        set({weatherList: []});
+      },
+    }),
+    {
+      name: 'multi-weather-store',
+    },
+  ),
+);
